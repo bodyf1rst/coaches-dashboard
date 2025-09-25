bb492d5ec08a864514e9629e4f7668f43be013e9
BodyF1RST <77579835+bodyf1rst@users.noreply.github.com>
2025-09-25 13:46:57 -0500

    Update API URL to working Elastic Beanstalk environment - 13:46 CST

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://awseb-e-y-AWSEBLoa-1Q1RHBK0D3Q0K-327736023.us-east-1.elb.amazonaws.com/api'
+  apiUrl: 'http://awseb-e-q-AWSEBLoa-13KGFBL7R8KWS-1984160231.us-east-1.elb.amazonaws.com/api'
 };
